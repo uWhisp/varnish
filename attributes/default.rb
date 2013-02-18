e55eb59a3768659c54575d9a23f3ac9583dd6883
Iuri Aranda <iuri.aranda@uwhisp.com>
2013-02-18 19:58:02 +0100

    Amazon Linux has the default config files on /etc/sysconfig/
    TODO: probably other distros too

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,10 @@
 case platform
-when "debian","ubuntu","amazon"
+when "debian","ubuntu"
   set['varnish']['dir']     = "/etc/varnish"
   set['varnish']['default'] = "/etc/default/varnish"
+when "amazon"
+  set['varnish']['dir']     = "/etc/varnish"
+  set['varnish']['default'] = "/etc/sysconfig/varnish"
 end
 
 default['varnish']['version'] = "2.1"
